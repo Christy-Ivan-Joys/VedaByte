e3dcea871654843f1bbd4a529f44a148989913b8
Christy-Ivan-Joys <christyivanjoys@gmail.com>
2024-08-30 12:08:49 +0530

    success-test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-console.log(process.env.NODE_ENV,'this isthe env pro')
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: process.env.NODE_ENV === 'production'
-          ? 'https://vedabyte.christyivanjoys.live'
-          : 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: mode === 'production'
+            ? 'https://vedabyte.christyivanjoys.live'
+            : 'http://localhost:5000',
+          changeOrigin: true,
+        }
       }
     }
   }
